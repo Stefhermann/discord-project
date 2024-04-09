81ec083ad4a34d100e94939419d05c5ec7341204
Stefan Hermann <herma912@umn.edu>
2024-04-09 09:22:19 -0500

    Manage Members

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,7 +1,7 @@
 import { Server } from "@prisma/client";
 import { create } from "zustand";
 
-export type ModalType = "create-server" | "invite" | "editServer";
+export type ModalType = "create-server" | "invite" | "editServer" | "members";
 
 interface ModalData {
   server?: Server;
