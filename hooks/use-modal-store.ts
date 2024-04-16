4f047e1f41f0b74b5774ad6e129bbcf971c0bb9b
Stefan Hermann <herma912@umn.edu>
2024-04-16 12:01:26 -0500

    Creating Channel

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,7 +1,12 @@
 import { Server } from "@prisma/client";
 import { create } from "zustand";
 
-export type ModalType = "create-server" | "invite" | "editServer" | "members";
+export type ModalType =
+  | "create-server"
+  | "invite"
+  | "editServer"
+  | "members"
+  | "createChannel";
 
 interface ModalData {
   server?: Server;
